05772d01ddc7aba1d8dea5facc726a86f5dc19b1
Ashutoshay <66373102+Ashutoshay@users.noreply.github.com>
2021-02-15 19:34:04 +0530

    Add files via upload

---
+++ CedHosting (1).sql
@@ -0,0 +1,345 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Feb 15, 2021 at 03:01 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.3.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `CedHosting`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_blog`
+--
+
+CREATE TABLE `tbl_blog` (
+  `id` int(11) NOT NULL,
+  `blog_title` varchar(44) NOT NULL,
+  `blog_desc` text NOT NULL,
+  `blog_date` datetime NOT NULL,
+  `author_name` varchar(44) NOT NULL DEFAULT 'Anonymous',
+  `caption_img` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_company_info`
+--
+
+CREATE TABLE `tbl_company_info` (
+  `id` int(11) NOT NULL,
+  `comp_name` varchar(55) NOT NULL,
+  `comp_logo` varchar(1000) NOT NULL,
+  `comp_contact` varchar(33) NOT NULL,
+  `comp_email` varchar(33) NOT NULL,
+  `comp_address` varchar(88) NOT NULL,
+  `comp_city` varchar(44) NOT NULL,
+  `comp_state` int(11) NOT NULL,
+  `comp_pincode` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_orders`
+--
+
+CREATE TABLE `tbl_orders` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `user_billing_id` int(11) NOT NULL,
+  `order_date` datetime NOT NULL,
+  `status` int(11) NOT NULL,
+  `promocode_applied_id` int(11) NOT NULL,
+  `discount_amt` float NOT NULL,
+  `total_amt_after_dis` float NOT NULL,
+  `tax_amt` float NOT NULL,
+  `final_invoice_amt` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_product`
+--
+
+CREATE TABLE `tbl_product` (
+  `id` int(11) NOT NULL,
+  `prod_parent_id` int(11) NOT NULL,
+  `prod_name` varchar(100) NOT NULL,
+  `html` longtext DEFAULT NULL,
+  `prod_available` tinyint(1) NOT NULL,
+  `prod_launch_date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_product`
+--
+
+INSERT INTO `tbl_product` (`id`, `prod_parent_id`, `prod_name`, `html`, `prod_available`, `prod_launch_date`) VALUES
+(1, 0, 'Hosting', NULL, 1, '2020-12-09 14:34:49'),
+(12, 1, 'Linux Hosting', 'linuxhosting.php', 1, '2021-01-13 11:29:24'),
+(15, 1, 'Windows Hosting', 'windowshosting.php', 1, '2021-01-13 11:32:29'),
+(18, 12, 'superhosting', 'abc.com', 1, '2021-01-15 09:56:44'),
+(19, 1, 'Mac Hosting', '', 1, '2021-02-06 11:14:59'),
+(20, 19, 'Mac74', 'Mac.com', 1, '2021-02-06 11:16:10');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_product_description`
+--
+
+CREATE TABLE `tbl_product_description` (
+  `id` int(11) NOT NULL,
+  `prod_id` int(11) NOT NULL,
+  `description` text DEFAULT NULL,
+  `mon_price` float NOT NULL,
+  `annual_price` float NOT NULL,
+  `sku` varchar(55) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_product_description`
+--
+
+INSERT INTO `tbl_product_description` (`id`, `prod_id`, `description`, `mon_price`, `annual_price`, `sku`) VALUES
+(1, 18, '{\"webspace\":\"5\",\"bandwidth\":\"2\",\"freedomain\":\"0\",\"languagetechnology\":\"PHP\",\"mailbox\":\"0\"}', 25, 250, 'sdg'),
+(2, 20, '{\"webspace\":\"5\",\"bandwidth\":\"2\",\"freedomain\":\"0\",\"languagetechnology\":\"PHP\",\"mailbox\":\"0\"}', 25, 250, 'sdg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_promocode`
+--
+
+CREATE TABLE `tbl_promocode` (
+  `id` int(11) NOT NULL,
+  `type` varchar(50) NOT NULL,
+  `value` varchar(22) NOT NULL,
+  `max_discount` int(11) NOT NULL,
+  `code` varchar(55) NOT NULL,
+  `expiry_date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_services`
+--
+
+CREATE TABLE `tbl_services` (
+  `id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `activation_time` datetime NOT NULL,
+  `tenure` char(1) NOT NULL,
+  `expiry_time` datetime NOT NULL,
+  `prod_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_state`
+--
+
+CREATE TABLE `tbl_state` (
+  `id` int(11) NOT NULL,
+  `name` varchar(55) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_user`
+--
+
+CREATE TABLE `tbl_user` (
+  `id` int(11) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `mobile` varchar(15) DEFAULT NULL,
+  `email_approved` tinyint(1) DEFAULT 0,
+  `phone_approved` tinyint(1) DEFAULT 0,
+  `active` tinyint(1) NOT NULL DEFAULT 0,
+  `is_admin` tinyint(1) NOT NULL,
+  `sign_up_date` datetime DEFAULT current_timestamp(),
+  `password` varchar(100) NOT NULL,
+  `security_question` varchar(100) DEFAULT NULL,
+  `security_answer` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_user`
+--
+
+INSERT INTO `tbl_user` (`id`, `email`, `name`, `mobile`, `email_approved`, `phone_approved`, `active`, `is_admin`, `sign_up_date`, `password`, `security_question`, `security_answer`) VALUES
+(1, 'ashutoshay2505@gmail.com', 'Admin', '8866745679', 1, 1, 1, 1, '2021-01-12 09:50:18', '1234', 'Name of Admin', 'Ashu'),
+(2, 'techtushargupta@gmail.com', 'Tushar', '6745678934', 1, 0, 1, 0, '2021-01-12 10:24:49', '123', 'What is the name of your favourite childhood friend?', 'Mahi'),
+(3, 'Khan@gmail.com', 'Danish', '8601197890', 1, 1, 1, 0, '2021-01-14 13:40:30', 'da87ae2c63280d1edc0051e62860f0ac', 'What is the name of your favourite childhood friend?', 'Mahi');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_user_billing_add`
+--
+
+CREATE TABLE `tbl_user_billing_add` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `billing_name` varchar(55) NOT NULL,
+  `house_no` varchar(55) NOT NULL,
+  `city` varchar(55) NOT NULL,
+  `state` int(11) NOT NULL,
+  `country` varchar(55) NOT NULL,
+  `pincode` varchar(55) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_blog`
+--
+ALTER TABLE `tbl_blog`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_company_info`
+--
+ALTER TABLE `tbl_company_info`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_orders`
+--
+ALTER TABLE `tbl_orders`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_product`
+--
+ALTER TABLE `tbl_product`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_product_description`
+--
+ALTER TABLE `tbl_product_description`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_promocode`
+--
+ALTER TABLE `tbl_promocode`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_services`
+--
+ALTER TABLE `tbl_services`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_state`
+--
+ALTER TABLE `tbl_state`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_user_billing_add`
+--
+ALTER TABLE `tbl_user_billing_add`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_blog`
+--
+ALTER TABLE `tbl_blog`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_company_info`
+--
+ALTER TABLE `tbl_company_info`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_orders`
+--
+ALTER TABLE `tbl_orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_product`
+--
+ALTER TABLE `tbl_product`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT for table `tbl_product_description`
+--
+ALTER TABLE `tbl_product_description`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tbl_promocode`
+--
+ALTER TABLE `tbl_promocode`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_services`
+--
+ALTER TABLE `tbl_services`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_state`
+--
+ALTER TABLE `tbl_state`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `tbl_user_billing_add`
+--
+ALTER TABLE `tbl_user_billing_add`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
